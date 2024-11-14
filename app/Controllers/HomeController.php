8217610445d04465344d664aa57130d44ccaeb0f
olaoluwa-wiseki <olaoluwa@wiseki.com>
2024-11-14 09:56:37 +0100

    updates

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -13,30 +13,30 @@ class HomeController extends Controller {
     public function index() {
         $revenue = [
             'total' => format_money(Transaction::where('amount','>', 1)->sum('amount')),
-            'percentage' => get_percentage_difference('transactions_tb', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1]], '7 day', 'mysql2', 't_date', true)
+            'percentage' => get_percentage_difference('transactions', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1]], '7 day', 'mysql2', 't_date', true)
         ];
         $subs = [
             'total' => number_format(Transaction::where('amount','>', 1)->where('charges_status','Success')->where('bearer_id','SecureD')->count()),
-            'percentage' => get_percentage_difference('transactions_tb', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'], ['column'=> 'bearer_id', 'value'=> 'SecureD']], '7 day', 'mysql2', 't_date', true, true),
+            'percentage' => get_percentage_difference('transactions', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'], ['column'=> 'bearer_id', 'value'=> 'SecureD']], '7 day', 'mysql2', 't_date', true, true),
         ];
         $unSubs = [
             // 'total' => number_format(SubDetail::where('sub_status', 0)->count()),
             'total' => number_format(Transaction::where('amount','>', 0)->where('charges_status', 'You deactivate the service successfully.')->count()),
-            'percentage' => get_percentage_difference('transactions_tb', '*', [['column' => 'charges_status', 'value'=>'You deactivate the service successfully.']], '7 day', 'mysql2', 't_date', true, true),
+            'percentage' => get_percentage_difference('transactions', '*', [['column' => 'charges_status', 'value'=>'You deactivate the service successfully.']], '7 day', 'mysql2', 't_date', true, true),
             // 'percentage' => get_percentage_difference('sub_details', '*', [['column' => 'sub_status', 'value'=>0]], '7 day', 'mysql2', 't_date', true, true),
         ];
         $subRev = [
             'total' => number_format(Transaction::where('amount','>', 1)->where('charges_status','Success')->where('bearer_id','<>','system-renewal')->sum('amount')),
-            'percentage' => get_percentage_difference('transactions_tb', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'], ['column'=> 'bearer_id', 'operator' => '<>', 'value'=> 'system-renewal']], '7 day', 'mysql2', 't_date', true, true),
+            'percentage' => get_percentage_difference('transactions', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'], ['column'=> 'bearer_id', 'operator' => '<>', 'value'=> 'system-renewal']], '7 day', 'mysql2', 't_date', true, true),
         ];
         $renRev = [
             'total' => number_format(Transaction::where('amount','>', 1)->where('charges_status','Success')->where('bearer_id','=','system-renewal')->sum('amount')),
-            'percentage' => get_percentage_difference('transactions_tb', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'],['column' => 'bearer_id', 'value' => 'system-renewal']], '7 day', 'mysql2', 't_date', true, true),
+            'percentage' => get_percentage_difference('transactions', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'],['column' => 'bearer_id', 'value' => 'system-renewal']], '7 day', 'mysql2', 't_date', true, true),
         ];
 
         $churnRate = [
             'total' => number_format(Transaction::where('amount','>', 1)->where('charges_status','Success')->where('bearer_id','=','system-renewal')->sum('amount')),
-            'percentage' => get_percentage_difference('transactions_tb', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'], ['column'=> 'bearer_id', 'value'=> 'system-renewal']], '7 day', 'mysql2', 't_date', true, true),
+            'percentage' => get_percentage_difference('transactions', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'], ['column'=> 'bearer_id', 'value'=> 'system-renewal']], '7 day', 'mysql2', 't_date', true, true),
         ];
         view('home', compact('revenue', 'subs', 'unSubs', 'subRev', 'renRev', 'churnRate'));
     }
@@ -46,35 +46,35 @@ class HomeController extends Controller {
             'stats' => [
                 'revenue' => [
                     'total' => format_money(Transaction::where('amount','>', 1)->sum('amount')),
-                    'percentage' => get_percentage_difference('transactions_tb', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1]], '7 day', 'mysql2', 't_date', true)
+                    'percentage' => get_percentage_difference('transactions', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1]], '7 day', 'mysql2', 't_date', true)
                 ],
                 'subs' => [
                     'total' => number_format(Transaction::where('amount','>', 1)->where('charges_status','Success')->where('bearer_id','SecureD')->count()),
-                    'percentage' => get_percentage_difference('transactions_tb', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'], ['column'=> 'bearer_id', 'value'=> 'SecureD']], '7 day', 'mysql2', 't_date', true, true),
+                    'percentage' => get_percentage_difference('transactions', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'], ['column'=> 'bearer_id', 'value'=> 'SecureD']], '7 day', 'mysql2', 't_date', true, true),
                 ],
                 'unSubs' => [
                     'total' => number_format(Transaction::where('amount','>', 0)->where('charges_status', 'You deactivate the service successfully.')->count()),
-                    'percentage' => get_percentage_difference('transactions_tb', 'amount', [['column' => 'charges_status', 'value'=>'You deactivate the service successfully.']], '7 day', 'mysql2', 't_date', true, true),
+                    'percentage' => get_percentage_difference('transactions', 'amount', [['column' => 'charges_status', 'value'=>'You deactivate the service successfully.']], '7 day', 'mysql2', 't_date', true, true),
                 ],
                 'subRev' => [
                     'total' => number_format(Transaction::where('amount','>', 1)->where('charges_status','Success')->where('bearer_id','<>','system-renewal')->sum('amount')),
-                    'percentage' => get_percentage_difference('transactions_tb', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'], ['column'=> 'bearer_id', 'operator' => '<>', 'value'=> 'system-renewal']], '7 day', 'mysql2', 't_date', true, true),
+                    'percentage' => get_percentage_difference('transactions', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'], ['column'=> 'bearer_id', 'operator' => '<>', 'value'=> 'system-renewal']], '7 day', 'mysql2', 't_date', true, true),
                 ],
                 'renRev' => [
                     'total' => number_format(Transaction::where('amount','>', 1)->where('charges_status','Success')->where('bearer_id','=','system-renewal')->sum('amount')),
-                    'percentage' => get_percentage_difference('transactions_tb', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'],['column' =>   'bearer_id', 'value' => 'system-renewal']], '7 day', 'mysql2', 't_date', true, true),
+                    'percentage' => get_percentage_difference('transactions', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'],['column' =>   'bearer_id', 'value' => 'system-renewal']], '7 day', 'mysql2', 't_date', true, true),
                 ],
                 'churnRate' => [
                     'total' => number_format(Transaction::where('amount','>', 1)->where('charges_status','Success')->where('bearer_id','=','system-renewal')->sum('amount')),
-                    'percentage' => get_percentage_difference('transactions_tb', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'], ['column'=> 'bearer_id', 'value'=> 'system-renewal']], '7 day', 'mysql2', 't_date', true, true),
+                    'percentage' => get_percentage_difference('transactions', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'], ['column'=> 'bearer_id', 'value'=> 'system-renewal']], '7 day', 'mysql2', 't_date', true, true),
                 ],
             ],
             'graphs' => [
-                'revenue' => get_interval_data('transactions_tb', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1]], 'day', 7, 'mysql2', 't_date', false ),
-                'subscriptions' => get_interval_data('transactions_tb', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'], ['column'=> 'bearer_id', 'value'=> 'SecureD']], 'day', 7, 'mysql2', 't_date', true, ),
-                'unSubs' => get_interval_data('transactions_tb', 'amount', [['column' => 'charges_status', 'value'=>'You deactivate the service successfully.']], 'day', 7, 'mysql2', 't_date', true, ),
-                'subRev' => get_interval_data('transactions_tb', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'], ['column'=> 'bearer_id', 'operator' => '<>', 'value'=> 'system-renewal']], 'day', 7, 'mysql2', 't_date', true, ),
-                'renRev' => get_interval_data('transactions_tb', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'],['column' =>   'bearer_id', 'value' => 'system-renewal']], 'day', 7, 'mysql2', 't_date', true, ),
+                'revenue' => get_interval_data('transactions', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1]], 'day', 7, 'mysql2', 't_date', false ),
+                'subscriptions' => get_interval_data('transactions', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'], ['column'=> 'bearer_id', 'value'=> 'SecureD']], 'day', 7, 'mysql2', 't_date', true, ),
+                'unSubs' => get_interval_data('transactions', 'amount', [['column' => 'charges_status', 'value'=>'You deactivate the service successfully.']], 'day', 7, 'mysql2', 't_date', true, ),
+                'subRev' => get_interval_data('transactions', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'], ['column'=> 'bearer_id', 'operator' => '<>', 'value'=> 'system-renewal']], 'day', 7, 'mysql2', 't_date', true, ),
+                'renRev' => get_interval_data('transactions', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'],['column' =>   'bearer_id', 'value' => 'system-renewal']], 'day', 7, 'mysql2', 't_date', true, ),
             ]
         ];
         return json_success($data);
@@ -87,35 +87,35 @@ class HomeController extends Controller {
             'stats' =>[
                 'revenue' => [
                     'total' => format_money($query->sum('amount')),
-                    'percentage' => get_percentage_difference('transactions_tb', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1], ['column'=> 'trans_id', 'operator'=> 'LIKE', 'value' => "$campaignAgency->code%"]], '7 day', 'mysql2', 't_date', true)
+                    'percentage' => get_percentage_difference('transactions', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1], ['column'=> 'trans_id', 'operator'=> 'LIKE', 'value' => "$campaignAgency->code%"]], '7 day', 'mysql2', 't_date', true)
                 ],
                 'subs' => [
                     'total' => number_format($query->where('charges_status','Success')->where('bearer_id','SecureD')->count()),
-                    'percentage' => get_percentage_difference('transactions_tb', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'], ['column'=> 'bearer_id', 'value'=> 'SecureD'], ['column'=> 'trans_id', 'operator'=> 'LIKE', 'value' => "$campaignAgency->code%"]], '7 day', 'mysql2', 't_date', true, true),
+                    'percentage' => get_percentage_difference('transactions', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'], ['column'=> 'bearer_id', 'value'=> 'SecureD'], ['column'=> 'trans_id', 'operator'=> 'LIKE', 'value' => "$campaignAgency->code%"]], '7 day', 'mysql2', 't_date', true, true),
                 ],
                 'unSubs' => [
                     'total' => number_format($query->where('charges_status', 'You deactivate the service successfully.')->count()),
-                    'percentage' => get_percentage_difference('transactions_tb', 'amount', [['column' => 'charges_status', 'value'=>'You deactivate the service successfully.'], ['column'=> 'trans_id', 'operator'=> 'LIKE', 'value' => "$campaignAgency->code%"]], '7 day', 'mysql2', 't_date', true, true),
+                    'percentage' => get_percentage_difference('transactions', 'amount', [['column' => 'charges_status', 'value'=>'You deactivate the service successfully.'], ['column'=> 'trans_id', 'operator'=> 'LIKE', 'value' => "$campaignAgency->code%"]], '7 day', 'mysql2', 't_date', true, true),
                 ],
                 'subRev' => [
                     'total' => number_format($query->where('charges_status','Success')->where('bearer_id','<>','system-renewal')->sum('amount')),
-                    'percentage' => get_percentage_difference('transactions_tb', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'], ['column'=> 'bearer_id', 'operator' => '<>', 'value'=> 'system-renewal'], ['column'=> 'trans_id', 'operator'=> 'LIKE', 'value' => "$campaignAgency->code%"]], '7 day', 'mysql2', 't_date', true, true),
+                    'percentage' => get_percentage_difference('transactions', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'], ['column'=> 'bearer_id', 'operator' => '<>', 'value'=> 'system-renewal'], ['column'=> 'trans_id', 'operator'=> 'LIKE', 'value' => "$campaignAgency->code%"]], '7 day', 'mysql2', 't_date', true, true),
                 ],
                 'renRev' => [
                     'total' => number_format($query->where('charges_status','Success')->where('bearer_id','=','system-renewal')->sum('amount')),
-                    'percentage' => get_percentage_difference('transactions_tb', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'],['column' =>   'bearer_id', 'value' => 'system-renewal'], ['column'=> 'trans_id', 'operator'=> 'LIKE', 'value' => "$campaignAgency->code%"]], '7 day', 'mysql2', 't_date', true, true),
+                    'percentage' => get_percentage_difference('transactions', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'],['column' =>   'bearer_id', 'value' => 'system-renewal'], ['column'=> 'trans_id', 'operator'=> 'LIKE', 'value' => "$campaignAgency->code%"]], '7 day', 'mysql2', 't_date', true, true),
                 ],
                 'churnRate' => [
                     'total' => number_format($query->where('charges_status','Success')->where('bearer_id','=','system-renewal')->sum('amount')),
-                    'percentage' => get_percentage_difference('transactions_tb', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'], ['column'=> 'bearer_id', 'value'=> 'system-renewal'], ['column'=> 'trans_id', 'operator'=> 'LIKE', 'value' => "$campaignAgency->code%"]], '7 day', 'mysql2', 't_date', true, true),
+                    'percentage' => get_percentage_difference('transactions', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'], ['column'=> 'bearer_id', 'value'=> 'system-renewal'], ['column'=> 'trans_id', 'operator'=> 'LIKE', 'value' => "$campaignAgency->code%"]], '7 day', 'mysql2', 't_date', true, true),
                 ],
             ],
             'graphs' => [
-                'revenue' => get_interval_data('transactions_tb', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1]], 'day', 7, 'mysql2', 't_date', false ),
-                'subscriptions' => get_interval_data('transactions_tb', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'], ['column'=> 'bearer_id', 'value'=> 'SecureD']], 'day', 7, 'mysql2', 't_date', true, ),
-                'unSubs' => get_interval_data('transactions_tb', 'amount', [['column' => 'charges_status', 'value'=>'You deactivate the service successfully.']], 'day', 7, 'mysql2', 't_date', true, ),
-                'subRev' => get_interval_data('transactions_tb', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'], ['column'=> 'bearer_id', 'operator' => '<>', 'value'=> 'system-renewal']], 'day', 7, 'mysql2', 't_date', true, ),
-                'renRev' => get_interval_data('transactions_tb', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'],['column' =>   'bearer_id', 'value' => 'system-renewal']], 'day', 7, 'mysql2', 't_date', true, ),
+                'revenue' => get_interval_data('transactions', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1]], 'day', 7, 'mysql2', 't_date', false ),
+                'subscriptions' => get_interval_data('transactions', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'], ['column'=> 'bearer_id', 'value'=> 'SecureD']], 'day', 7, 'mysql2', 't_date', true, ),
+                'unSubs' => get_interval_data('transactions', 'amount', [['column' => 'charges_status', 'value'=>'You deactivate the service successfully.']], 'day', 7, 'mysql2', 't_date', true, ),
+                'subRev' => get_interval_data('transactions', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'], ['column'=> 'bearer_id', 'operator' => '<>', 'value'=> 'system-renewal']], 'day', 7, 'mysql2', 't_date', true, ),
+                'renRev' => get_interval_data('transactions', 'amount', [['column'=> 'amount', 'operator' => '>', 'value'=> 1],['column'=> 'charges_status', 'value'=> 'Success'],['column' =>   'bearer_id', 'value' => 'system-renewal']], 'day', 7, 'mysql2', 't_date', true, ),
             ]
         ];
         return json_success($data);
